a45d9047a499b1ed906acc2c87e5c7b71fec3ac4
khabane <prodigitallabinc@gmail.com>
2023-03-20 11:40:22 -0400

    Generating Spring controllers/models from the specification

--- src/main/java/com/tawfekh/apiRestMongoDb/controller/StudentController.java
+++ src/main/java/com/tawfekh/apiRestMongoDb/controller/StudentController.java
@@ -1,25 +1,18 @@
 package com.tawfekh.apiRestMongoDb.controller;
 
-import com.tawfekh.apiRestMongoDb.mapper.StudentMapper;
 import com.tawfekh.apiRestMongoDb.service.StudentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.tawfekh.mongodbservice.api.ApiApi;
-import org.tawfekh.mongodbservice.models.api.resource.Address;
+import org.tawfekh.mongodbservice.api.StudentApi;
 import org.tawfekh.mongodbservice.models.api.resource.Student;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
-public class StudentController implements ApiApi {
+public class StudentController implements StudentApi {
     private final StudentService studentService;
 
     @Override
